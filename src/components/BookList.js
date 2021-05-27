479256ba9a3250b3f6b15361e7846ca8fddcd5a9
bigwizzo <wnnwillard@gmail.com>
2021-05-27 22:46:03 +0200

    Display all books in state

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,25 +1,40 @@
+/* eslint-disable react/forbid-prop-types */
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-const BookList = () => (
-  <div>
-    <h1 className="text-center">Center</h1>
-    <table className="table table-striped">
-      <thead>
-        <tr>
-          <th scope="col">Book ID</th>
-          <th scope="col">Title</th>
-          <th scope="col">Category</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>ID-1</td>
-          <td>Title One</td>
-          <td>Category One</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-);
+const BookList = ({ books }) => {
+  console.log(books);
+  return (
+    <div>
+      <h1 className="text-center">Center</h1>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">Book ID</th>
+            <th scope="col">Title</th>
+            <th scope="col">Category</th>
+          </tr>
+        </thead>
+        <tbody>
+          {books.map((book) => (
+            <tr key={book.id}>
+              <td>{book.id}</td>
+              <td>{book.title}</td>
+              <td>{book.category}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
-export default BookList;
+BookList.propTypes = {
+  books: PropTypes.array.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  books: state.books,
+});
+export default connect(mapStateToProps)(BookList);
