10c728cc4f5613dfac7fa3b8a6deab446ac0585d
bigwizzo <wnnwillard@gmail.com>
2021-05-27 22:06:46 +0200

    Add the CREATE_BOOK and REMOVE_BOOK actions

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+const CREATE_BOOK = 'CREATE_BOOK';
+const REMOVE_BOOK = 'REMOVE_BOOK';
+
+export const createBookAction = () => ({
+  type: CREATE_BOOK,
+});
+
+export const cremoveBookAction = () => ({
+  type: REMOVE_BOOK,
+});
