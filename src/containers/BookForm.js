741ea21a40fa62aa3c548a27c9161ac1fd842da4
bigwizzo <wnnwillard@gmail.com>
2021-05-28 20:33:56 +0200

    Connect form to redux

--- src/containers/BookForm.js
+++ src/containers/BookForm.js
@@ -1,5 +1,10 @@
 /* eslint-disable react/no-array-index-key */
 import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { createBookAction } from '../actions';
+
+const uniqid = require('uniqid');
 
 const cat = [
   'Select category',
@@ -12,7 +17,7 @@ const cat = [
   'Sci-Fi',
 ];
 
-const BookForm = () => {
+const BookForm = ({ createBook }) => {
   const [title, setTitle] = useState('');
   const [category, setCategory] = useState('');
 
@@ -28,7 +33,8 @@ const BookForm = () => {
       alert('please add a category');
       return;
     }
-
+    const book = { id: uniqid.process(), title, category };
+    createBook(book);
     console.log(title, category);
 
     setTitle('');
@@ -39,10 +45,21 @@ const BookForm = () => {
     <div className="container d-flex justify-content-center">
       <form onSubmit={onSubmit}>
         <div className="form-group align-self-center">
-          <input type="text" className="form-control p-2 m-3" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Type the books title" />
+          <input
+            type="text"
+            className="form-control p-2 m-3"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder="Type the books title"
+          />
         </div>
         <div className="form-group align-self-center">
-          <select className="form-select p-2 m-3" value={category} onChange={(e) => setCategory(e.target.value)} aria-label="Default select example">
+          <select
+            className="form-select p-2 m-3"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+            aria-label="Default select example"
+          >
             {cat.map((x, i) => (
               <option value={x} key={i}>
                 {x}
@@ -58,4 +75,14 @@ const BookForm = () => {
   );
 };
 
-export default BookForm;
+BookForm.propTypes = {
+  createBook: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = (dispatch) => {
+  const createBook = (book) => dispatch(createBookAction(book));
+
+  return { createBook };
+};
+
+export default connect(null, mapDispatchToProps)(BookForm);
